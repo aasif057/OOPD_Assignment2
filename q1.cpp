9a7abd3346b558e3f542350c70a16a1a226fa25b
aasif057 <mdaasifxii@gmail.com>
2024-10-01 05:17:17 +0530

    file updated

--- q1.cpp
+++ q1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-
+#include <cctype>
 using namespace std;
 
-// Base class for Staff
-class Staff {
+// Base class for Faculty
+class Faculty {
 public:
     char name[100];
     char email[100];
-    char room[20];
-    char phone[20];
+    char room[100];
+    char phone[50];
     char position[50];
 
-    Staff(const char* n, const char* e, const char* r, const char* p) {
+    Faculty(const char* n, const char* e, const char* r, const char* p) {
         strcpy(name, n);
         strcpy(email, e);
         strcpy(room, r);
@@ -21,145 +21,189 @@ public:
         strcpy(position, p);
     }
 
-    virtual ~Staff() {}  // Virtual destructor
-};
-
-// Registrar class
-class Registrar : public Staff {
-public:
-    Registrar(const char* n, const char* e, const char* r, const char* p) 
-        : Staff(n, e, r, p) {}
-};
-
-// Dean class
-class Dean : public Staff {
-public:
-    Dean(const char* n, const char* e, const char* r, const char* p) 
-        : Staff(n, e, r, p) {}
-};
-
-// Associate Dean class
-class AssociateDean : public Staff {
-public:
-    AssociateDean(const char* n, const char* e, const char* r, const char* p) 
-        : Staff(n, e, r, p) {}
+    virtual ~Faculty() {}  // Virtual destructor
 };
 
 // Administrative Officer class
-class AdministrativeOfficer : public Staff {
+class AdministrativeOfficer : public Faculty {
 public:
     AdministrativeOfficer(const char* n, const char* e, const char* r, const char* p) 
-        : Staff(n, e, r, p) {}
+        : Faculty(n, e, r, p) {}
 };
 
-// Junior Administrative Officer class
-class JuniorAdministrativeOfficer : public Staff {
+// Head class
+class Head : public Faculty {
 public:
-    JuniorAdministrativeOfficer(const char* n, const char* e, const char* r, const char* p) 
-        : Staff(n, e, r, p) {}
+    Head(const char* n, const char* e, const char* r, const char* p) 
+        : Faculty(n, e, r, p) {}
 };
 
-// Administrative Department class
-class AdministrativeDepartment {
+// Professor class
+class Professor : public Faculty {
+public:
+    Professor(const char* n, const char* e, const char* r, const char* p) 
+        : Faculty(n, e, r, p) {}
+};
+
+// Associate Professor class
+class AssociateProfessor : public Faculty {
+public:
+    AssociateProfessor(const char* n, const char* e, const char* r, const char* p) 
+        : Faculty(n, e, r, p) {}
+};
+
+// Assistant Professor class
+class AssistantProfessor : public Faculty {
+public:
+    AssistantProfessor(const char* n, const char* e, const char* r, const char* p) 
+        : Faculty(n, e, r, p) {}
+};
+
+// Academics Departments class
+class AcademicsDepartment {
 private:
-    Registrar* registrar; // Pointer to the department's registrar
-    Dean* dean; // Pointer to the department's dean
-    AssociateDean* associateDean; // Pointer to the department's associate dean
-    AdministrativeOfficer* officers[10]; // Array for administrative officers
-    JuniorAdministrativeOfficer* juniorOfficers[10]; // Array for junior officers
+    AdministrativeOfficer* administrativeOfficer; // Pointer to the Administrative Officer
+    Head* head; // Pointer to department Head
+    Professor* professors[20]; // Array for Professors
+    AssociateProfessor* associateProfessors[20]; // Array for Associate Professors
+    AssistantProfessor* assistantProfessors[20]; // Array for Assistant Professors
 
-    int officerCount;
-    int juniorOfficerCount;
+    int professorCount;
+    int associateCount;
+    int assistantCount;
 
 public:
-    AdministrativeDepartment() 
-        : registrar(nullptr), dean(nullptr), associateDean(nullptr), officerCount(0), juniorOfficerCount(0) {}
+    AcademicsDepartment() 
+        : administrativeOfficer(nullptr), head(nullptr), professorCount(0), associateCount(0), assistantCount(0) {}
 
-    void setRegistrar(Registrar* reg) {
-        registrar = reg;
+    void setAdministrativeOfficer(AdministrativeOfficer* ao) {
+        administrativeOfficer = ao;
     }
 
-    void setDean(Dean* d) {
-        dean = d;
+    void setHead(Head* h) {
+        head = h;
     }
 
-    void setAssociateDean(AssociateDean* ad) {
-        associateDean = ad;
-    }
-
-    void addOfficer(AdministrativeOfficer* officer) {
-        if (officerCount < 10) {
-            officers[officerCount++] = officer;
+    void addProfessor(Professor* p) {
+        if (professorCount < 20) {
+            professors[professorCount++] = p;
         } else {
-            cout << "Max number of officers reached!" << endl;
+            cout << "Max number of Professors reached!" << endl;
         }
     }
 
-    void addJuniorOfficer(JuniorAdministrativeOfficer* officer) {
-        if (juniorOfficerCount < 10) {
-            juniorOfficers[juniorOfficerCount++] = officer;
+    void addAssociateProfessor(AssociateProfessor* associate) {
+        if (associateCount < 20) {
+            associateProfessors[associateCount++] = associate;
         } else {
-            cout << "Max number of junior officers reached!" << endl;
+            cout << "Max number of Associate Professors reached!" << endl;
+        }
+    }
+
+    void addAssistantProfessor(AssistantProfessor* assistant) {
+        if (assistantCount < 20) {
+            assistantProfessors[assistantCount++] = assistant;
+        } else {
+            cout << "Max number of Assistant Professors reached!" << endl;
         }
     }
 
     // Method to display department members with hierarchy
     void displayMembers() const {
         cout << "Department Members:" << endl;
-        if (registrar) {
-            cout << "Registrar: " << registrar->name << endl;
+        if (administrativeOfficer) {
+            cout << "Administrative Officer: " << administrativeOfficer->name << endl;
         }
-        if (dean) {
-            cout << "Dean: " << dean->name << endl;
+        if (head) {
+            cout << "Head: " << head->name << endl;
         }
-        else{
-            cout << "No Dean" << endl;
+        for (int i = 0; i < professorCount; i++) {
+            cout << "Professor: " << professors[i]->name << endl;
         }
-        if (associateDean) {
-            cout << "Associate Dean: " << associateDean->name << endl;
+        for (int i = 0; i < associateCount; i++) {
+            cout << "Associate Professor: " << associateProfessors[i]->name << endl;
         }
-        else{
-            cout << "No Associate Dean" << endl;
+        for (int i = 0; i < assistantCount; i++) {
+            cout << "Assistant Professor: " << assistantProfessors[i]->name << endl;
         }
-        for (int i = 0; i < officerCount; i++) {
-            cout << "Administrative Officer: " << officers[i]->name << endl;
-        }
-        for (int i = 0; i < juniorOfficerCount; i++) {
-            cout << "Junior Administrative Officer: " << juniorOfficers[i]->name << endl;
-        }
-        cout << endl; // Add a line break for better readability
+        cout << "-----------------------------------------------------------" << endl; // Add a line break for better readability
     }
+
+    // Destructor to clean up dynamically allocated objects
+    ~AcademicsDepartment() {
+        delete administrativeOfficer;
+        delete head;
+        for (int i = 0; i < professorCount; i++) {
+            delete professors[i];
+        }
+        for (int i = 0; i < associateCount; i++) {
+            delete associateProfessors[i];
+        }
+        for (int i = 0; i < assistantCount; i++) {
+            delete assistantProfessors[i];
+        }
+    }
+
     bool searchMember(const char* nameToSearch) const {
-        if (registrar && strcmp(registrar->name, nameToSearch) == 0) {
-            cout << "Name: " << registrar->name << ", Email: " << registrar->email << ", Room: " << registrar->room << ", Phone: " << registrar->phone << endl;
+        if (administrativeOfficer && strcmp(administrativeOfficer->name, nameToSearch) == 0) {
+            cout << "Name: " << administrativeOfficer->name << ", Email: " << administrativeOfficer->email 
+                 << ", Room: " << administrativeOfficer->room << ", Phone: " << administrativeOfficer->phone << endl;
             return true;
         }
-        if (dean && strcmp(dean->name, nameToSearch) == 0) {
-            cout << "Name: " << dean->name << ", Email: " << dean->email << ", Room: " << dean->room << ", Phone: " << dean->phone << endl;
+        if (head && strcmp(head->name, nameToSearch) == 0) {
+            cout << "Name: " << head->name << ", Email: " << head->email 
+                 << ", Room: " << head->room << ", Phone: " << head->phone << endl;
             return true;
         }
-        if (associateDean && strcmp(associateDean->name, nameToSearch) == 0) {
-            cout << "Name: " << associateDean->name << ", Email: " << associateDean->email << ", Room: " << associateDean->room << ", Phone: " << associateDean->phone << endl;
-            return true;
-        }
-        for (int i = 0; i < officerCount; i++) {
-            if (strcmp(officers[i]->name, nameToSearch) == 0) {
-                cout << "Name: " << officers[i]->name << ", Email: " << officers[i]->email << ", Room: " << officers[i]->room << ", Phone: " << officers[i]->phone << endl;
+        for (int i = 0; i < professorCount; i++) {
+            if (strcmp(professors[i]->name, nameToSearch) == 0) {
+                cout << "Name: " << professors[i]->name << ", Email: " << professors[i]->email 
+                     << ", Room: " << professors[i]->room << ", Phone: " << professors[i]->phone << endl;
                 return true;
             }
         }
-        for (int i = 0; i < juniorOfficerCount; i++) {
-            if (strcmp(juniorOfficers[i]->name, nameToSearch) == 0) {
-                cout << "Name: " << juniorOfficers[i]->name << ", Email: " << juniorOfficers[i]->email << ", Room: " << juniorOfficers[i]->room << ", Phone: " << juniorOfficers[i]->phone << endl;
+        for (int i = 0; i < associateCount; i++) {
+            if (strcmp(associateProfessors[i]->name, nameToSearch) == 0) {
+                cout << "Name: " << associateProfessors[i]->name << ", Email: " << associateProfessors[i]->email 
+                     << ", Room: " << associateProfessors[i]->room << ", Phone: " << associateProfessors[i]->phone << endl;
+                return true;
+            }
+        }
+        for (int i = 0; i < assistantCount; i++) {
+            if (strcmp(assistantProfessors[i]->name, nameToSearch) == 0) {
+                cout << "Name: " << assistantProfessors[i]->name << ", Email: " << assistantProfessors[i]->email 
+                     << ", Room: " << assistantProfessors[i]->room << ", Phone: " << assistantProfessors[i]->phone << endl;
                 return true;
             }
         }
         return false;
     }
 };
+void trim(char* str) {
+    // Remove leading spaces
+    char* start = str;
+    while (isspace((unsigned char)*start)) start++;
 
+    // All spaces, set the string to an empty one
+    if (*start == 0) {
+        *str = 0;
+        return;
+    }
+
+    // Remove trailing spaces
+    char* end = start + strlen(start) - 1;
+    while (end > start && isspace((unsigned char)*end)) end--;
+
+    // Null-terminate the string after the last non-space character
+    end[1] = '\0';
+
+    // Move the trimmed string to the beginning
+    if (start != str) {
+        memmove(str, start, end - start + 2);
+    }
+}
 // Function to load data from CSV file for a specific department
-void loadAdministrativeData(const char* filename, AdministrativeDepartment& department, const char* departmentName) {
+void loadAdministrativeData(const char* filename, AcademicsDepartment& department, const char* departmentName) {
     ifstream file(filename);
     if (!file.is_open()) {
         cout << "Failed to open file: " << filename << endl;
@@ -170,21 +214,19 @@ void loadAdministrativeData(const char* filename, AdministrativeDepartment& depa
     file.getline(line, 256); // Skip header
 
     while (file.getline(line, 256)) {
-        char name[100], email[100], room[500], phone[100], position[200], dept[100];
-        // cout << line << endl;
-        // Updated sscanf to read 6 columns including department
-        sscanf(line, "%99[^,],%99[^,],%499[^,],%99[^,],%199[^,],%99[^\n]", name, email, room, phone, position, dept);
-
-        // Only process staff if they belong to the correct department
+        char name[300], email[200], room[500], phone[200], position[300], dept[100];
+        sscanf(line, "%299[^,],%199[^,],%499[^,],%199[^,],%299[^,],%99[^\n]", name, email, room, phone, position, dept);
+        trim(dept);
         if (strcmp(dept, departmentName) == 0) {
-            if (strcmp(position, "Dean") == 0) {
-                department.setDean(new Dean(name, email, room, phone));
-            } else if (strcmp(position, "Associate Dean") == 0) {
-                department.setAssociateDean(new AssociateDean(name, email, room, phone));
-            } else if (strcmp(position, "Administrative Officer") == 0 || strcmp(position, "Library Officer") == 0) {
-                department.addOfficer(new AdministrativeOfficer(name, email, room, phone));
-            } else if (strcmp(position, "Junior Administrative Officer") == 0 || strcmp(position, "Junior Library Officer") == 0) {
-                department.addJuniorOfficer(new JuniorAdministrativeOfficer(name, email, room, phone));
+            cout << "Yes" << endl;
+            if (strcmp(position, "Head") == 0) {
+                department.setHead(new Head(name, email, room, phone));
+            } else if (strcmp(position, "Professor") == 0) {
+                department.addProfessor(new Professor(name, email, room, phone));
+            } else if (strcmp(position, "Associate Professor") == 0) {
+                department.addAssociateProfessor(new AssociateProfessor(name, email, room, phone));
+            } else if (strcmp(position, "Assistant Professor") == 0) {
+                department.addAssistantProfessor(new AssistantProfessor(name, email, room, phone));
             }
         }
     }
@@ -192,67 +234,53 @@ void loadAdministrativeData(const char* filename, AdministrativeDepartment& depa
 
 int main() {
     // Create departments
-    AdministrativeDepartment irdDepartment;
-    AdministrativeDepartment academicsDepartment;
-    AdministrativeDepartment studentAffairsDepartment;
-    AdministrativeDepartment storePurchaseDepartment;
-    AdministrativeDepartment libraryDepartment;
+    AcademicsDepartment eceDepartment;
+    AcademicsDepartment cseDepartment;
+    AcademicsDepartment sshDepartment;
+    AcademicsDepartment mathDepartment;
+    AcademicsDepartment cbDepartment;
+    AcademicsDepartment hcdDepartment;
 
-    Registrar* registrar = new Registrar("Dr. Deepika Bhaskar", "deepika@iiitd.ac.in", "Room No. B-204 (Academic Block)", "+91-11-26907419");
-    
-    // Set registrar for each department
-    irdDepartment.setRegistrar(registrar);
-    academicsDepartment.setRegistrar(registrar);
-    studentAffairsDepartment.setRegistrar(registrar);
-    storePurchaseDepartment.setRegistrar(registrar);
-    libraryDepartment.setRegistrar(registrar);
+    //Add Administrative Officer
+    AdministrativeOfficer* administrativeOfficer = new AdministrativeOfficer("Sumit J Darak", "doaa@iiitd.ac.in", "NA", "+91-11-26907427");
+    //Add Administrative Officer to every Department
+    eceDepartment.setAdministrativeOfficer(administrativeOfficer);
+    cseDepartment.setAdministrativeOfficer(administrativeOfficer);
+    sshDepartment.setAdministrativeOfficer(administrativeOfficer);
+    mathDepartment.setAdministrativeOfficer(administrativeOfficer);
+    cbDepartment.setAdministrativeOfficer(administrativeOfficer);
+    hcdDepartment.setAdministrativeOfficer(administrativeOfficer);
 
     // Load data from CSV for each department
-    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiit_admin_data.csv", irdDepartment, "IRD");
-    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiit_admin_data.csv", academicsDepartment, "Academics");
-    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiit_admin_data.csv", studentAffairsDepartment, "Student Affairs");
-    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiit_admin_data.csv", storePurchaseDepartment, "Store & Purchase");
-    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiit_admin_data.csv", libraryDepartment, "Library");
+    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiitd_faculty_data.csv", eceDepartment, "ECE");
+    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiitd_faculty_data.csv", cseDepartment, "CSE");
+    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiitd_faculty_data.csv", sshDepartment, "SSH");
+    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiitd_faculty_data.csv", mathDepartment, "Math");
+    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiitd_faculty_data.csv", cbDepartment, "CB");
+    loadAdministrativeData("/home/aasif057/Documents/OOPD/OOPD_Assignment2/iiitd_faculty_data.csv", hcdDepartment, "HCD");
 
     // Display department members
     cout << "Administrative Structure:\n" << endl;
-    cout << "IRD" << endl;
-    irdDepartment.displayMembers();
-    cout << "Academics" << endl;
-    academicsDepartment.displayMembers();
-    cout << "Student Affairs" << endl;
-    studentAffairsDepartment.displayMembers();
-    cout << "Store & Purchase" << endl;
-    storePurchaseDepartment.displayMembers();
-    cout << "Library" << endl;
-    libraryDepartment.displayMembers();
-    
+    cout << "ECE Department" << endl;
+    eceDepartment.displayMembers();
+    cout << "CSE Department" << endl;
+    cseDepartment.displayMembers();
+    cout << "SSH Department" << endl;
+    sshDepartment.displayMembers();
+    cout << "Math Department" << endl;
+    mathDepartment.displayMembers();
 
-    // Search A Staff
     char nameToSearch[100];
-    cout << "Enter the name of the staff member to search: ";
+    cout << "Enter the name of the Faculty member to search: ";
     cin.getline(nameToSearch, 100);
 
     // Search through all departments
-    if (irdDepartment.searchMember(nameToSearch)) {
+    if (eceDepartment.searchMember(nameToSearch) || cseDepartment.searchMember(nameToSearch) || 
+        sshDepartment.searchMember(nameToSearch) || mathDepartment.searchMember(nameToSearch) || 
+        cbDepartment.searchMember(nameToSearch) || hcdDepartment.searchMember(nameToSearch)) {
         cout << "Found" << endl;
+    } else {
+        cout << nameToSearch << " not found in any department!" << endl;
     }
-    else if (academicsDepartment.searchMember(nameToSearch)) {
-        cout << "Found" << endl;
-    }
-    else if (studentAffairsDepartment.searchMember(nameToSearch)) {
-        cout << "Found" << endl;
-    }
-    else if (storePurchaseDepartment.searchMember(nameToSearch)) {
-        cout << "Found" << endl;
-    }
-    else if (libraryDepartment.searchMember(nameToSearch)) {
-        cout << "Found" << endl;
-    }
-    else {
-        cout << nameToSearch << "' not found in any department!" << endl;
-    }
-
-    delete registrar;
     return 0;
 }
